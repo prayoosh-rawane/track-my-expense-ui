7559a26ac0a8d2708e886eb8c2ac4ef9ca1063a4
Prayoosh Rawane <p.rawane@gmail.com>
2025-08-06 09:01:45 +0530

    inital setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+      <Typography variant='h1' component='h1'>
+        Welcome to Track My Expense!
+      </Typography>
+    </>
+  )
+}
+
+export default App
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+// src/theme.ts
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+
+})
+
+export default theme
